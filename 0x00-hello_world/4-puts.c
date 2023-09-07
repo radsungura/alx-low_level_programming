dadb5aff53bb64c824f1055b811e9bff7274c669
Radsungura <rukundaime@gmail.com>
2023-09-08 01:31:20 +0200

    My fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * prints with "Puts"
- *    */
+ * main-prints with "Puts",
+ * the text will be on the screen,
+ * Return 0 at the end
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
