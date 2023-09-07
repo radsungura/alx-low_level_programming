5a20aa2b9b6e3604d77f9a00640e2c259eebc5df
Radsungura <rukundaime@gmail.com>
2023-09-08 01:53:48 +0200

    My sixth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints with "printf"
- * Principal function
+ * main - prints the phrase with proper grammar,
+ * simple principal fuction,,
  * Return: 0 at the end
- */ 
-
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
-
