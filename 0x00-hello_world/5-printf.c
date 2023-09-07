5f071aaceae12abe5cb491c5be6d117b044a06ca
Radsungura <rukundaime@gmail.com>
2023-09-08 01:24:03 +0200

    My sixth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - prints with "printf"
-* main function,
-* Return: 0
-*/ 
+ * main - prints with "printf"
+ * main function,
+ * Return: 0
+ */ 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
