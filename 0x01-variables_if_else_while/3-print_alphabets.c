feba9562838fb258cbe7b84bccdaff0ff7b81061
Radsungura <rukundaime@gmail.com>
2023-09-11 20:42:00 +0200

    My fourth commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,8 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-		for (ch = 'A'; ch <= 'Z'; ch++)
-			putchar(ch);
-		putchar('\n');
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
 	return (0);
 }
