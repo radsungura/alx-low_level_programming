6678ff26aeac365840e5cf601391c4bf56bb5dcc
Radsungura <rukundaime@gmail.com>
2023-09-13 01:32:11 +0200

    My sixth commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: is the variable to check
- * Return: 1  if n is greater than zero, 
+ * Return: 1 if n is greater than zero,
  * 0 if n is zero, -1  if n is less than zero
  */
 int print_sign(int n)
