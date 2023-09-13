4281d63f0309ebad43f78ad4c1e28eae61859942
Radsungura <rukundaime@gmail.com>
2023-09-13 02:14:42 +0200

    My eleventh commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result
+ * @x: first int 
+ * @y: second int 
+ * Return: the sum 
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -8,3 +8,4 @@
 7. prints the last digit of a number.
 8. prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
 9. prints the 9 times table, starting with 0.
+10. adds two integers and returns the result.
