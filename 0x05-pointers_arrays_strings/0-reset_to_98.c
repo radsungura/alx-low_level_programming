524ddfe0b62556507dfac8293dcb0b8fefaf9d1e
Radsungura <rukundaime@gmail.com>
2023-09-18 14:53:02 +0200

    My first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates to 98 the value of the variable
+ * pointed
+ * @n: pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
