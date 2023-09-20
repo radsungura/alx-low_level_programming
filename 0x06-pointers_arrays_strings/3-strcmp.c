673eae91dc3ea2fcccec87390a11532b4874ca7e
Radsungura <rukundaime@gmail.com>
2023-09-20 18:25:00 +0200

    My fourth commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,8 @@
 
 /**
  * _strcmp - compares two strings
- * @s1: first string 
+ * @s1: first string
  * @s2: second string
- *
  * Return: less than 0 if 's1' is less than 's2', 0 if they're equal,
  * more than 0 if 's1' is greater than 's2'
  */
