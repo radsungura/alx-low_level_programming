2849f9648a829012187252fb1de3979da73b4014
Radsungura <rukundaime@gmail.com>
2023-09-26 04:27:51 +0200

    My second commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,19 +2,20 @@
 
 /**
  * *_memcpy - copies memory area
- * @dest: memory area of destination,
- * @src: memory area to copy from,
- * @n: number of bytes to be copied,
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
  *
- * * Return: a pointer to 'dest'
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
+
 	return (dest);
 }
