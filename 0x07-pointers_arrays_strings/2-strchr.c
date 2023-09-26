d7b1844fdb219c9750396c90a099d601c1656333
Radsungura <rukundaime@gmail.com>
2023-09-26 03:17:33 +0200

    My second commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,7 +7,7 @@
  * @c: char to find
  *
  * Return: a pointer to the first occurrence
- * of the character c in the string s, 
+ * of the character c in the string s,
  * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
