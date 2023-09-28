83d8fbbd4d01ddc75a3bb830d8d8c7f26912735c
Radsungura <rukundaime@gmail.com>
2023-09-29 00:33:23 +0200

    My sixth commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,6 +15,10 @@ int _sqrt_recursion(int n)
 	return (actual_sqrt_recursion(n, 0));
 }
 
+/**
+ * actual_sqrt_recursion - get find the natural square root of a number
+ */
+
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
