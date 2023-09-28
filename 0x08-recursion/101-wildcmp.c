c2175f37600287374312d4e466e90cf945c83068
Radsungura <rukundaime@gmail.com>
2023-09-29 01:08:05 +0200

    My nineth commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * wildcmp - compares two strings and returns 1 if the strings 
+ * wildcmp - compares two strings and returns 1 if the strings
  * can be considered identical,
  * otherwise return 0.
  * @s1: the normal string
