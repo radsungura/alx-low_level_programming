67cedcb94da9ee2eabc7a5d175310759272a1a02
Radsungura <rukundaime@gmail.com>
2023-10-03 18:15:26 +0200

    My second commit

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase
+ * @c: character to check
+ * Return: 1 if c is uppercase,otherwise 1
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
