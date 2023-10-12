56b20917de83d3f411b1a2210bee0bc54168cd9d
Radsungura <rukundaime@gmail.com>
2023-10-12 20:49:21 +0200

    My Error fix commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,8 +2,8 @@
 
 /** 
  * print_numbers - prints numbers, followed by a new line.
- * @separator : is the string to be printed between numbers
- * @n : number of numbers passed
+ * @separator: is the string to be printed between numbers
+ * @n: number of numbers passed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
