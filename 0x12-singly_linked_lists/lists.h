01bce4189f7129671dd3adb3250030d931e6b246
rad5001 <rukundaime@gmail.com>
2023-10-19 13:47:47 +0200

    fix lists.h comments

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
- * struct list_s - list singly linked
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: string lenght
- * @next: pointer for the next
+ * @len: length of the string
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
