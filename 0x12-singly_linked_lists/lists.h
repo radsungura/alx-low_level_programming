93070530500b958f6b2b984298a71e23105663d5
rad5001 <rukundaime@gmail.com>
2023-10-19 23:15:44 +0200

    My sixth commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,6 @@ size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void first(void);
+void first(void) __attribute__ ((constructor));
 
 #endif
