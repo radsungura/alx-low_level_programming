6f4c36c2c37c01f61d9717739de757e8a7670503
rad5001 <rukundaime@gmail.com>
2023-10-25 06:45:13 +0200

    Fix space error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on 
+ * array_iterator - executes a function given as a parameter on
  * each element of an array.
  * @array: array to iterate on
  * @size: size of the array
