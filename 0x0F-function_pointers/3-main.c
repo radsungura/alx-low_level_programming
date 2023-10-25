1b1c88ba2decc3eb67e26ccc6bd75cdc4c95af5e
rad5001 <rukundaime@gmail.com>
2023-10-25 06:52:08 +0200

    Fix space error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
 	arg2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
+
 	if (!func)
 	{
 		printf("Error\n");
@@ -36,7 +37,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
+
 	result = func(arg1, arg2);
+
 	printf("%d\n", result);
 
 	return (0);
